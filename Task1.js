90381bbacd85ae4db6b716f81cedc7a9a60089ad
riskijanuar <108819936+riskijanuar@users.noreply.github.com>
2022-07-06 22:34:51 +0700

    Add files via upload

---
+++ Task1.js
@@ -0,0 +1,49 @@
+const biodata = {
+    name : "Riski Januar Ramadlon",
+    age : 23,
+    hobbies : ['Basketball', 'Gaming', 'Singing'] ,
+    isMarried : false,
+    schoolList : [
+        {
+            "name" : "SDN Selong 04 Pagi Jaksel",
+            "yearIn" : 2005,
+            "yearOut" : 2011,
+            "major" : null
+        },
+        {
+            "name" : "SMP Purnama 1 Jaksel",
+            "yearIn" : 2011,
+            "yearOut" : 2014,
+            "major" : null
+        },
+        {
+            "name" : "SMK Purnama 1 Jaksel",
+            "yearIn" : 2014,
+            "yearOut" : 2017,
+            "major" : "Multimedia"
+        },
+        {
+            "name" : "Universitas Budi Luhur",
+            "yearIn" : 2017,
+            "yearOut" : 2021,
+            "major" : "Teknik Informatika"
+        }
+    ],
+    skill: [
+        {
+            "skillName" : "coding javascript",
+            "level" : "beginner",
+        },
+        {
+            "skillName" : "design with corel draw",
+            "level" : "advanced",
+        },
+        {
+            "skillName" : "editing video with adobe premiere pro",
+            "level" : "advanced",
+        }
+    ],
+    interstInCoding : true
+}
+
+console.log(biodata)
